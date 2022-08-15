9ac91cf8da3c7a3ccad5115925a2df28a877c2a0
Shabnam Zare <shabnamzaresh@gmail.com>
2022-08-15 14:14:21 +0430

    refactored;

--- 3-finance-concepts/financial_concepts.py
+++ 3-finance-concepts/financial_concepts.py
@@ -8,23 +8,29 @@ def compute_daily_return(data):
     return data
 
 
-def compute_daily_sharpe_ratio(data, risk_free=0):
+def compute_sharpe_ratio(data, risk_free_rate=0, duration='daily'):
+    durations = {
+        'daily': 1,
+        'weekly': 5,
+        'monthly': 21,
+        'annual': 252
+    }
     daily_return = compute_daily_return(data)['Daily Return']
     mean_return = daily_return.mean()
     std_return = daily_return.std()
 
-    sharpe_ratio = (mean_return - risk_free) / std_return
+    sharpe_ratio = ((mean_return - risk_free_rate) / std_return) * np.sqrt(durations[duration])
     return sharpe_ratio
 
 
-def compute_annual_sharpe_ratio(data, risk_free=0):
-    daily_sharpe_ratio = compute_daily_sharpe_ratio(data, risk_free)
-    annual_sharpe_ratio = daily_sharpe_ratio * np.sqrt(252)
-    return annual_sharpe_ratio
-
-
-def compute_probabilistic_sr(data, benchmark=0, risk_free=0):
-    daily_sharpe_ratio = compute_daily_sharpe_ratio(data, risk_free)
+def compute_probabilistic_sharpe_ratio(data, benchmark=0, risk_free_rate=0, duration='daily'):
+    durations = {
+        'daily': 1,
+        'weekly': 5,
+        'monthly': 21,
+        'annual': 252
+    }
+    daily_sharpe_ratio = compute_sharpe_ratio(data, risk_free_rate)
     daily_return = compute_daily_return(data)['Daily Return']
     daily_return = np.array(daily_return)
 
@@ -34,28 +40,22 @@ def compute_probabilistic_sr(data, benchmark=0, risk_free=0):
 
     std_sr = np.sqrt((1 / (n - 1)) * (1 + (0.5 * np.square(daily_sharpe_ratio)) - (skew * daily_sharpe_ratio) + (kurtosis / 4) * np.square(daily_sharpe_ratio)))
     ratio = (daily_sharpe_ratio - benchmark) / std_sr
-    psr = stats.norm.cdf(ratio)
+    psr = stats.norm.cdf(ratio) * np.sqrt(durations[duration])
 
     return psr
 
 
-def compute_annual_psr(data, benchmark=0, risk_free=0):
-    daily_psr = compute_probabilistic_sr(data, benchmark, risk_free)
-    annual_psr = daily_psr * np.sqrt(252)
-    return annual_psr
-
-
-def compute_daily_sortino_ratio(data, threshold=0, risk_free=0):
+def compute_sortino_ratio(data, threshold=0, risk_free_rate=0, duration='daily'):
+    durations = {
+        'daily': 1,
+        'weekly': 5,
+        'monthly': 21,
+        'annual': 252
+    }
     daily_return = compute_daily_return(data)['Daily Return']
     mean_return = daily_return.mean()
     downsides = daily_return[daily_return < threshold]
     std_return = downsides.std()
 
-    sartino_ratio = (mean_return - risk_free) / std_return
+    sartino_ratio = ((mean_return - risk_free_rate) / std_return) * np.sqrt(durations[duration])
     return sartino_ratio
-
-
-def compute_annual_sortino_ratio(data, threshold=0, risk_free=0):
-    daily_ratio = compute_daily_sortino_ratio(data, threshold, risk_free)
-    annual_ratio = daily_ratio * np.sqrt(252)
-    return annual_ratio
