e6ff52d00c4c79254214b8acaad93922723fdbef
Shabnam Zare <shabnamzaresh@gmail.com>
2022-08-16 12:34:35 +0430

    eda

--- 2-exploratory-data-analysis/financial_functions.py
+++ 2-exploratory-data-analysis/financial_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import stats
+import pandas as pd
+import os
 
 durations = {
         'daily': 1,
@@ -9,14 +11,19 @@ durations = {
     }
 
 
+def compute_moving_average(data, duration='monthly'):
+    moving_average = data.rolling(durations[duration]).mean()
+    return moving_average
+
+
 def compute_daily_return(data):
-    data['Daily Return'] = data['Adj Close'].pct_change(1)
-    data.dropna(inplace=True)
-    return data
+    daily_returns = data.pct_change(1)
+    daily_returns.dropna(axis=0, inplace=True)
+    return daily_returns
 
 
 def compute_sharpe_ratio(data, risk_free_rate=0, duration='daily'):
-    daily_return = compute_daily_return(data)['Daily Return']
+    daily_return = compute_daily_return(data['Adj Close'])
     mean_return = daily_return.mean()
     std_return = daily_return.std()
 
@@ -26,7 +33,7 @@ def compute_sharpe_ratio(data, risk_free_rate=0, duration='daily'):
 
 def compute_probabilistic_sharpe_ratio(data, benchmark=0, risk_free_rate=0, duration='daily'):
     daily_sharpe_ratio = compute_sharpe_ratio(data, risk_free_rate)
-    daily_return = compute_daily_return(data)['Daily Return']
+    daily_return = compute_daily_return(data['Adj Close'])
     daily_return = np.array(daily_return)
 
     skew = stats.skew(daily_return)
@@ -41,10 +48,38 @@ def compute_probabilistic_sharpe_ratio(data, benchmark=0, risk_free_rate=0, dura
 
 
 def compute_sortino_ratio(data, threshold=0, risk_free_rate=0, duration='daily'):
-    daily_return = compute_daily_return(data)['Daily Return']
+    daily_return = compute_daily_return(data['Adj Close'])
     mean_return = daily_return.mean()
     downsides = daily_return[daily_return < threshold]
     std_return = downsides.std()
 
     sartino_ratio = ((mean_return - risk_free_rate) / std_return) * np.sqrt(durations[duration])
     return sartino_ratio
+
+
+def compute_cumulative_return(data):
+    cum_return = (1 + data).cumprod() - 1
+    return cum_return
+
+
+def compute_cumulative_percent_return(data):
+    cum_return = compute_cumulative_return(data)
+    cum_perc_return = cum_return * 100
+    return cum_perc_return
+
+
+def import_all_files(path):
+    companies = {}
+    for file in os.listdir(path):
+        if file.endswith('.csv'):
+            name = file.split('.')[0]
+            companies[name] = pd.read_csv(os.path.join(path, file), index_col='Date', parse_dates=True)
+
+    # data = pd.concat(companies, axis=0)
+    # data = pd.DataFrame(companies)
+    return companies
+
+
+def import_single_file(path, symbol):
+    data = pd.read_csv(os.path.join(path, symbol + '.csv'), index_col='Date', parse_dates=True)
+    return data
---
+++ 2-exploratory-data-analysis/visualization_functions.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from financial_functions import *
+sns.color_palette("husl", 8)
+
+
+def plot_feature(data, feature):
+    plt.figure(figsize=(16, 8))
+    line_plot = sns.lineplot(x=data.index, y=data[feature])
+    line_plot.set_title(f'{feature}', fontdict={'fontsize':16}, pad=16)
+
+    plt.show()
+
+
+def plot_correlations(data):
+    plt.figure(figsize=(16, 8))
+    mask = np.triu(np.ones_like(data.corr(), dtype=bool))
+    heatmap = sns.heatmap(data.corr(), mask=mask, vmin=-1, vmax=1, annot=True, cmap='coolwarm')
+    heatmap.set_title('Correlation Heatmap', fontdict={'fontsize': 16}, pad=16)
+
+    plt.show()
+
+
+def plot_moving_average(data, duration='monthly'):
+    data = compute_moving_average(data, duration)
+    plt.figure(figsize=(16, 8))
+    sns.lineplot(x=data.index, y=data['Close'])
+
+    plt.show()
